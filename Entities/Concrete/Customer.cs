589e769381ead03550db5757b2542a4f36bb342c
sümeyra sağır <sumeyrasagir2017@outlook.com>
2021-02-13 19:44:36 +0300

    Add project files.

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+   public class Customer:IEntity
+    {
+        public string CustomerId { get; set; } //genel kullanım string olduğu için böyle verdik
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+   public class Product:IEntity //bu classa diğer katmanlar da ulaşabilsin diye public dedik
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
